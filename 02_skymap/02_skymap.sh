b1f83d8077a3b3a81f25f260f950d76e36843bc5
Hsin-Fang Chiang <hchiang@lsst.org>
2020-01-31 18:00:58 -0600

    Fix the commas, add visit IDs, add skymap

---
+++ 02_skymap/02_skymap.sh
@@ -0,0 +1,3 @@
+makeSkyMap.py /datasets/hsc/repo --calib /datasets/hsc/calib/20200115 --rerun DM-23243/SFM/UDEEP 
+makeSkyMap.py /datasets/hsc/repo --calib /datasets/hsc/calib/20200115 --rerun DM-23243/SFM/DEEP 
+makeSkyMap.py /datasets/hsc/repo --calib /datasets/hsc/calib/20200115 --rerun DM-23243/SFM/WIDE
