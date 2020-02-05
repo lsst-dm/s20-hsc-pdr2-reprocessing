2707a14ec5a5a891bf13eaef60adfe2c0314d680
Hsin-Fang Chiang <hchiang@lsst.org>
2020-02-04 19:03:39 -0600

    Add DEEP+UDEEP filter/tract/visits for jointcal

---
+++ 03_jointcal/jointcal_template.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+ 
+#SBATCH -p normal
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=13-99:99:00
+#SBATCH -J jc@TRACT@-@FILTER@
+#SBATCH --output=/datasets/hsc/repo/rerun/DM-23243/logs/jointcal/DEEP/jointcal-@TRACT@-@FILTER@-%j.log
+#SBATCH --error=/datasets/hsc/repo/rerun/DM-23243/logs/jointcal/DEEP/jointcal-@TRACT@-@FILTER@-%j.log
+#SBATCH --reservation=pdr2
+
+
+srun jointcal.py /datasets/hsc/repo --calib /datasets/hsc/calib/20200115 --rerun DM-23243/SFM/DEEP:DM-23243/JOINTCAL/DEEP --id ccd=0..8^10..103 visit=@VISIT@ filter=@FILTER@ tract=@TRACT@ --config doPhotometry=False
